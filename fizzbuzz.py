578a30c2414751ef5282bdbbd4b97e821f3a2252
psjin2024p <psjin2024p@gmail.com>
2025-04-23 13:05:17 +0900

    feat: modify

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,6 @@
 #if문 1개로 fizzbuzz 구현하기(docs)
-for i in range(1, 2020202020202020202020202020202020202020 0 or i % 5 == 0:
+for i in range(1, 20+1)
+    if i % 3 == 0 or i % 5 == 0:
         print('Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0))
     else:
         print(f'{i}')
